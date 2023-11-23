21705f0885aac40a7e07292176edec89c3359b26
GavinOP <HaoxiangZhang@link.cuhk.edu.hk>
2023-11-23 14:37:50 +0800

    Update create_table.sql

--- sql_practice/create_table.sql
+++ sql_practice/create_table.sql
@@ -9,19 +9,41 @@ EXCUTION:
 */ 
 
 
-Spool create_table_20231122.lst
+Spool create_table_20231123.lst
+-- show existing tables
+SELECT table_name 
+FROM user_tables;
+
 -- create table
 CREATE TABLE students(
     sid     CHAR(20),
     name    CHAR(30),
     login   CHAR(20),
     age     INTEGER,
-    gpa     REAL
+    gpa     REAL,
+    UNIQUE (login),
+    CONSTRAINT studentKey PRIMARY KEY (sid)
+);
+
+CREATE TABLE courses(
+    cid     CHAR(20),
+    cname   CHAR(20),
+    credits INTEGER,
+    CONSTRAINT courseKey PRIMARY KEY (cid)
+);
+
+CREATE TABLE enrolled(
+    sid     CHAR(20),
+    cid     CHAR(20),
+    grade   CHAR(10),
+    PRIMARY KEY (sid, cid),
+    FOREIGN KEY (sid) REFERENCES students(sid) ON UPDATE CASCADE,
+    FOREIGN KEY (cid) REFERENCES courses(cid) ON UPDATE CASCADE
 );
 
 -- insert data
 INSERT INTO students (sid, name, login, age, gpa)
-VALUES (5688, 'Gavin', 'smith@ee', 18, 3.2);
+VALUES (1588, 'OP', '123@ee', 18, 3.2);
 
 -- show table
 SELECT * FROM students;
@@ -31,7 +53,22 @@ DELETE FROM students s
 WHERE s.name = 'Smith';
 
 -- update row
+UPDATE students S
+SET S.age = S.age + 1, S.gpa = S.gpa + 0.1
+WHERE S.name = 'Gavin';
+
+UPDATE students s
+SET S.gpa = S.gpa + 0.1
+WHERE S.gpa < 3.5;
+
+-- drop table
+DROP TABLE students;
+
+
+
+
 Spool off
+
 
 
 
